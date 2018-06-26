f1426fd86df2ffc09a5e1052f9ff8264654d78ac
Your Name <1218866243@qq.com>
2018-06-26 09:39:57 +0800

    添加注释

--- src/components/com-a.vue
+++ src/components/com-a.vue
@@ -9,12 +9,18 @@
 import { mapState } from 'vuex'
 
 export default {
+  // 假设当前组件中已经有count属性
+  // 属性 是要映视到当前组件中的属性名称 this.num
+  // 值: 是 vue 的仓库中管理的数据 this.$store.count
   // computed: mapState({
   // num: 'count'
   // })
 
   // this.count (这是一个计算属性) 映视到 this.$store.state.count
   computed: {
+    // 组件本身的计算属性,写于不写都可以,不知道为什么我写了就报错,但仍能正常执行
+    //test() {
+    //}
     ...mapState({
       num: 'count'
     })
